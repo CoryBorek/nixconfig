a78909888013060d8fa77e5744a4b99ba234f38f
Cory Borek <cmb14369@uga.edu>
2024-02-19 10:04:53 -0500

    test

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+g# Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -9,6 +9,9 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
