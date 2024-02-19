cc8ea2229c80b5527bd5db78218582e261dc8b74
Cory Borek <agentdid127cb@aol.com>
2024-02-19 07:23:31 -0500

    tes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -78,7 +78,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
   # services.qemuGuest.enable = true;
 
   # Enable GNOME Desktop
