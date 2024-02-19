915d091671f6fc84f3bb8efd2229b21c239ba4f3
Cory Borek <agentdid127cb@aol.com>
2024-02-19 06:16:38 -0500

    User Flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "My Nix Config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in
+  {
+    nixosConfigurations = {
+      cory = nixpkgs.lib.nixosSystem {
+        inherit system;
+	modules = [
+          ./nixos/configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+            home-manager = {
+              useUserPackages = true;
+	      useGlobalPkgs = true;
+	      users.cory = ./home-manager/home.nix;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
