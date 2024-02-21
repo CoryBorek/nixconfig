eecfe3a2067043e0153493e987a74a6ab5da25d8
Cory Borek <agentdid127cb@aol.com>
2024-02-20 21:46:45 -0500

    leave unstable behind

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "My Nix Config";
+  description = "Cory's Nix Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
