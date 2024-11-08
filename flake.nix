f6745dceae13d3cf613e26a759fe46728f12f1f1
Cory Borek <agentdid127cb@aol.com>
2024-11-08 09:59:09 -0500

    refactor home manager into a single desktop home

--- flake.nix
+++ flake.nix
@@ -17,48 +17,48 @@
     nixosConfigurations = {
       nixos-desktop = nixpkgs.lib.nixosSystem {
         inherit system;
-	modules = [
-	  ./nixos/configuration.nix
-  	  ./hosts/regular/configuration.nix
-	  ./modules/global/desktop-cinnamon.nix
-	  home-manager.nixosModules.home-manager
-	  {
+         modules = [
+          ./nixos/configuration.nix
+          ./hosts/desktop/desktop.nix
+          ./modules/global/desktop-cinnamon.nix
+          home-manager.nixosModules.home-manager
+          {
             home-manager = {
               useUserPackages = true;
-	      useGlobalPkgs = true;
-	      users.cborek = ./hosts/regular/home-manager/cory.nix;
+              useGlobalPkgs = true;
+              users.cborek = ./hosts/desktop/home-manager/cory.nix;
             };
           }
-	];
+        ];
       };
       nixos-kde-desktop = nixpkgs.lib.nixosSystem {
         inherit system;
-	modules = [
-	  ./nixos/configuration.nix
-  	  ./hosts/kde/configuration.nix
-	  ./modules/global/desktop-plasma.nix
-	  home-manager.nixosModules.home-manager
-	  {
+         modules = [
+          ./nixos/configuration.nix
+          ./hosts/desktop/kde-desktop.nix
+          ./modules/global/desktop-plasma.nix
+          home-manager.nixosModules.home-manager
+          {
             home-manager = {
               useUserPackages = true;
-	      useGlobalPkgs = true;
-	      users.cborek = ./hosts/regular/home-manager/cory.nix;
+              useGlobalPkgs = true;
+              users.cborek = ./hosts/desktop/home-manager/cory.nix;
             };
           }
-	];
+        ];
       };
       nixos-term = nixpkgs.lib.nixosSystem {
         inherit system;
-	modules = [
+	      modules = [
           ./nixos/configuration.nix
           ./hosts/term/configuration.nix
-	  ./modules/global/default.nix
-	  home-manager.nixosModules.home-manager
-	  {
+          ./modules/global/default.nix
+          home-manager.nixosModules.home-manager
+          {
             home-manager = {
               useUserPackages = true;
-	      useGlobalPkgs = true;
-	      users.cborek = ./hosts/term/home-manager/cory.nix;
+	            useGlobalPkgs = true;
+	            users.cborek = ./hosts/term/home-manager/cory.nix;
             };
           }
         ];
