160053f2b3c909268a8396930d2e152a3bf27e0e
Cory Borek <agentdid127cb@aol.com>
2024-11-15 00:06:33 -0500

    add xrdp to cinnamon for remote access

--- modules/global/desktop-cinnamon.nix
+++ modules/global/desktop-cinnamon.nix
@@ -6,4 +6,8 @@
 
   config.services.cinnamon.enable = true;
   config.display.enable = true;
+
+  config.services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "cinnamon";
+  networking.firewall.allowedTCPPorts = [ 3389 ];
 }
