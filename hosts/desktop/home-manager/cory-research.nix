f93e33241cceb8a10f4647ea5f4ffad5f3b5ae73
NixOS user <user@example.com>
2025-05-22 19:55:18 -0400

    test

--- hosts/desktop/home-manager/cory-research.nix
+++ hosts/desktop/home-manager/cory-research.nix
@@ -4,6 +4,7 @@
 
   imports = [
     ../../../modules/desktop.nix
+    ../../../modules/nixos-vscode-ssh-fix.nix
   ];
 # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -78,4 +79,5 @@
   programs.kitty.enable = true;
   programs.starship.enable = false;
   programs.home-manager.enable = true;
+  services.nixos-vscode-ssh-fix.enable = true;
 }
