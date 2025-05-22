243c57e83b9ea4e2fc2d23423f1c1f9e59b60808
NixOS user <user@example.com>
2025-05-22 19:42:52 -0400

    t

--- hosts/desktop/home-manager/cory-research.nix
+++ hosts/desktop/home-manager/cory-research.nix
@@ -4,7 +4,7 @@
 
   imports = [
     ../../../modules/desktop.nix
-    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+    (fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
   ];
 # Home Manager needs a bit of information about you and the paths it should
   # manage.
