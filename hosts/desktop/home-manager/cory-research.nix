82fd75d575ec13cd2267259adb7b5ce20226ebb8
NixOS user <user@example.com>
2025-05-22 20:00:41 -0400

    t

--- hosts/desktop/home-manager/cory-research.nix
+++ hosts/desktop/home-manager/cory-research.nix
@@ -4,6 +4,7 @@
 
   imports = [
     ../../../modules/desktop.nix
+    ../../../modules/nixos-vscode-ssh-fix.nix
   ];
 # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -43,7 +44,6 @@
     gh
     vscode
     chromium
-    openvscode-server
   ];
 
 
